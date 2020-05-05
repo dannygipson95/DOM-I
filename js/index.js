baa74d2f7ee2e5d4b5bcc946dcd3ad6845822ce7
Daniel Gipson <dannygipson95@gmail.com>
2020-05-05 10:03:46 -0400

    completed mvp

--- js/index.js
+++ js/index.js
@@ -40,3 +40,96 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let navItems = Array.from(document.querySelectorAll('nav'));
+navItems.forEach(item =>{
+  item.firstElementChild.textContent = siteContent.nav["nav-item-1"];
+
+  item.firstElementChild.nextElementSibling.textContent = siteContent.nav["nav-item-2"];
+
+  item.firstElementChild.nextElementSibling.nextElementSibling.textContent = siteContent.nav["nav-item-3"];
+
+  item.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.textContent = siteContent.nav["nav-item-4"];
+
+  item.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent = siteContent.nav["nav-item-5"];
+
+  item.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent = siteContent.nav["nav-item-6"];
+});
+
+let h1Item = document.querySelector('h1');
+h1Item.textContent = siteContent.cta.h1;
+
+let buttonItem = document.querySelector('button');
+buttonItem.textContent = siteContent.cta.button;
+
+let ctaImage = document.getElementById("cta-img");
+ctaImage.setAttribute('src', siteContent["cta"]["img-src"])
+
+let feath4 = Array.from(document.getElementsByClassName('top-content'));
+
+console.log(feath4);
+
+feath4.forEach(item =>{
+  item.firstElementChild.firstElementChild.textContent = siteContent["main-content"]["features-h4"];
+});
+
+feath4.forEach(item => {
+  item.firstElementChild.firstElementChild.nextElementSibling.textContent = siteContent["main-content"]["features-content"];
+});
+
+feath4.forEach(item => {
+  item.firstElementChild.nextElementSibling.firstElementChild.textContent = siteContent["main-content"]["about-h4"];
+});
+
+feath4.forEach(item => {
+  item.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.textContent = siteContent["main-content"]["about-content"];
+});
+
+let middleImage = document.getElementById('middle-img');
+middleImage.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+let bottomCont = Array.from(document.getElementsByClassName('bottom-content'));
+
+bottomCont.forEach(item => {
+  item.firstElementChild.firstElementChild.textContent = siteContent["main-content"]["services-h4"];
+
+  item.firstElementChild.firstElementChild.nextElementSibling.textContent = siteContent["main-content"]["services-content"];
+
+  item.firstElementChild.nextElementSibling.firstElementChild.textContent = siteContent["main-content"]["product-h4"];
+
+  item.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.textContent = siteContent["main-content"]["product-content"];
+
+  item.firstElementChild.nextElementSibling.nextElementSibling.firstElementChild.textContent = siteContent["main-content"]["vision-h4"];
+
+  item.firstElementChild.nextElementSibling.nextElementSibling.firstElementChild.nextElementSibling.textContent = siteContent["main-content"]["vision-content"];
+});
+
+let contact = Array.from(document.getElementsByClassName('contact'));
+
+contact.forEach(item => {
+  item.firstElementChild.textContent = siteContent.contact["contact-h4"];
+
+  item.firstElementChild.nextElementSibling.textContent = siteContent.contact.address;
+
+  item.firstElementChild.nextElementSibling.nextElementSibling.textContent = siteContent.contact.phone;
+
+  item.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.textContent = siteContent.contact.email;
+});
+
+let footer = Array.from(document.getElementsByTagName('footer'));
+
+footer.forEach(item => {
+  item.firstElementChild.textContent = siteContent.footer.copyright;
+});
+
+let anchors = Array.from(document.getElementsByTagName('a'));
+
+anchors.forEach(item => {
+  item.style.color = 'green';
+});
+
+document.body.firstElementChild.firstElementChild.firstElementChild.prepend('nav 1');
+
+document.body.firstElementChild.firstElementChild.firstElementChild.append('nav 2');
+
+
